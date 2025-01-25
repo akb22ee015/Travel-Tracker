96eea7623b16b345a30ef9d4867c0c139cf26c97
Atish Kumar <b22ee015@iitj.ac.in>
2025-01-25 22:49:55 +0530

    dotenv update

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "akb22ee015311",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public", (req, res, next) => {
+app.use(
+  express.static("public", (req, res, next) => {
     console.log(`Serving static file: ${req.url}`);
     next();
-  }));
-  
+  })
+);
 
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
